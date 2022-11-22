ef1289fcb84a35e37930e8ad5744a9e99b5d5547
SAJADnet <sajadsajad.sch@gmail.com>
2022-11-22 23:51:29 +0330

    some comments was added

--- pyneat.py
+++ pyneat.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 import time
 
-
+# A dictionary of most popular file types
 DIRECTORIES = {
     "HTML": [".html5", ".html", ".htm", ".xhtml"],
     "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
@@ -27,19 +27,22 @@ DIRECTORIES = {
   
 }
 
+
 FILE_FORMATS = {file_format: directory
                 for directory, file_formats in DIRECTORIES.items()
                 for file_format in file_formats}
 
 def organize_junk(dest_path):
+    # Attempts to change the current directory to the "dest_path" directory
     try:
         os.chdir(dest_path) 
     except Exception as e:
         print(e)
         sys.exit()
 
+    # Checks the files in the 'dest_path' directory and organizes them into different directories   
     for entry in os.scandir():
-        if entry.is_dir():
+        if entry.is_dir(): # Ignores the directories
             continue
         file_path = Path(entry)
         file_format = file_path.suffix.lower()
@@ -55,7 +58,9 @@ def organize_junk(dest_path):
                 pass
         
 if __name__ == "__main__":
+    # Checks the user arguments for dest_path
     if dest_path:=sys.argv[1]:
+        # Calculates the total process time
         start_time = time.time()
         organize_junk(dest_path)
         total_time = (time.time() - start_time)
